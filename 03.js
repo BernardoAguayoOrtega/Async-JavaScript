20501f16c4ad18ae72a93e528f925a08496de0b9
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-05 08:53:12 -0600

    Fetch API

---
+++ 03.js
@@ -0,0 +1,34 @@
+// Make Network Request with fetch()
+// API => Application Programming Interface: 
+// Software to communicate with other software
+
+// API => helpful service
+
+// REST API
+
+// CRUD => 
+// create => POST, 
+// read => GET,
+// update => PUT / PATCH, 
+// deleted => DELETE
+
+// JSON => JavaScript Object Notation
+// fetch(url => endpoint)
+
+// GET /posts/1 - single blog post
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+const blogPost = {
+  title: "I'm a title"
+}
+
+// Post
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+  method: "POST",
+  headers: {
+    "content-type": "application/jso" 
+  },
+  body: JSON.stringify(blogPost)
+})
